c05fdc583ef47dff15f666d654dfbca452060de9
omj1 <omj@o2.pl>
2017-02-18 13:08:57 +0100

    file JPG changed to jpg

--- js/change.js
+++ js/change.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
 	"use strict";
 	var i = 0,
 		myTimer,
-		images = ['3.jpg', '1.jpg', '5.jpg', '2.jpg'],
+		images = ['3a.jpg', '1.jpg', '5.jpg', '2.jpg'],
 		image = $('.div1'),
 		ImgPath = "images/bg/" //The file path to your images
                 //Initial Background image setup
 	image.css('background-image', 'url(images/bg/4.jpg)');
-	 
+
                 //Change image at regular intervals
     myTimer = setInterval(changeBackground, 6000);
-      
+
    function changeBackground() {
          image.fadeOut(1000, function () {
             image.css('background-image', 'url('+ ImgPath + images[i++] +')');
@@ -20,23 +20,22 @@ $(document).ready(function () {
             i = 0;
          }
    }
-	
+
 	  // Optional code to hide all divs
         $(".content").hide();
           // Show chosen div, and hide all others
        $(".one, .two, .three").click(function (e) {
          e.preventDefault();
          if($(this).is(".one") || $(this).is(".two")) {
-            if (myTimer) { 
+            if (myTimer) {
                clearInterval(myTimer);
             }
-         } 
+         }
          else {
             $(".content").hide();
             myTimer = setInterval(changeBackground, 6000);
          }
          $("#" + $(this).attr("class")).removeClass("hide").fadeIn("slow").siblings('.content').hide();
    });
-      
-});
 
+});
